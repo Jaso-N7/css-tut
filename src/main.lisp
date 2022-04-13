d372f514c39577f12802feda777eedf2af229a0f
Jason Robinson <json.robinson@gmail.com>
2022-04-13 00:19:41 -0500

    Server START and STOP functionality
    User can start and stop the webserver

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,22 @@
 (defpackage css-tut
-  (:use :cl))
+  (:use :cl :net.aserve)
+  (:export #:start
+	   #:stop))
+
 (in-package :css-tut)
 
-;; blah blah blah.
+(let ((webserver nil))
+  (defun start (&key (port 80))
+    "Starts Portable AllegroServe on PORT"
+    (setf webserver (net.aserve:start :port port))
+    (format t "~&(Portable) AServe started on :~A~%" port)
+    #+unix
+    (format t "With PID: ~A~%" (net.aserve::getpid))
+    webserver)
+
+  (defun stop (&key (server webserver))
+    "Stops the current Portable AllegroServer."
+    (net.aserve:shutdown :server server)
+    (print "(Portable) Aserve was shutdown.")
+    server))
+
