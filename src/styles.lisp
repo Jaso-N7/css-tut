a5f90d612578a8afede49b23c0b047fb0a8274a8
Jason Robinson <json.robinson@gmail.com>
2022-04-14 19:30:28 -0500

    Home page using Tron Legacy theme

--- src/styles.lisp
+++ src/styles.lisp
@@ -1,7 +1,8 @@
 (defpackage styles
   (:documentation "CSS for project")
   (:use :cl :lass)
-  (:export #:stylesheet))
+  (:export #:stylesheet
+	   #:homestyle))
 
 (in-package :styles)
 
@@ -14,3 +15,20 @@
      :color blue)
    '("#services"
      :color green)))
+
+(defun homestyle ()
+  "CSS for introduction page. Using Tron: Legacy pallete from
+https://www.colourlovers.com/palette/1406402/Tron_Legacy_2"
+  (lass:compile-and-write
+   '(body
+     :background-color "#0C141F"
+     :color "#E6FFFF"
+     :font-family "Helvetica, Arial, sans-serif")
+   '((:and "#yt-link" :link)
+     :color "#DF740C")
+   '((:and "#yt-link" :visited)
+     :color "#FFE64D")
+   '((:and "#tut-link" :link)
+     :color "#6FC3DF")
+   '((:and "#tut-link" :visited)
+     :color "#E6FFFF")))
